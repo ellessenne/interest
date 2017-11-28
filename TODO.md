5cca0c9d48daebba991bd318956d2b901f1599a9
Alessandro Gasparini <ag475@le.ac.uk>
2017-11-28 12:46:10 +0000

    Revamped data uploading form; adapted to using rsimsum; implemented showing and exporting summary tables

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
-# To-do list
+# To-do
 
-1. Switch to using `rsimsum`
 1. Implement `shiny::validate` to check whether a dataset has been uploaded or not
 1. Implement tables of summary values:
     * Show only a subset of summary values
@@ -9,3 +8,8 @@
     * Lollyplot
     * Estimate vs SE plot
 1. Allowing multiple estimands at once
+1. Prettier output tables
+
+# Bugs
+
+* Getting `Error: $ operator is invalid for atomic vectors` in the _Define variables:_ area when trying to link/paste files
--- server.R
+++ server.R
@@ -4,60 +4,121 @@ source("helpers.R")
 function(input, output, session) {
 
 	# Make data selectors on the fly
-	output$loadData = renderUI({
+	output$loadData = shiny::renderUI({
 		switch(input$typeDataUpload,
-					 "uploadData" = fileInput(inputId = "uploadData", label = NULL, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-					 "linkData" = textInput(inputId = "linkData", label = NULL),
-					 "pasteData" = textAreaInput(inputId = "pasteData", label = NULL))
+					 "uploadData" = shiny::fileInput(inputId = "uploadData", label = NULL),
+					 "linkData" = shiny::textInput(inputId = "linkData", label = NULL),
+					 "pasteData" = shiny::textAreaInput(inputId = "pasteData", label = NULL))
 	})
 
 	# Functions to load data
-	data = reactive({
-		df = switch(input$typeDataUpload,
-									"uploadData" = {
-										inFile = input$uploadData
-										if (is.null(inFile)) return(NULL)
-										read_csv(inFile$datapath)
-										},
-									"linkData" = {
-										inFile = input$linkData
-										if (is.null(inFile)) return(NULL)
-										read_csv(inFile)
-										},
-									"pasteData" = {
-										inFile = input$linkData
-										if (is.null(inFile)) return(NULL)
-										read_csv(inFile)
-										})
+	data = shiny::reactive({
+		inFile = switch(input$typeDataUpload,
+										"uploadData" = input$uploadData,
+										"linkData" = input$linkData,
+										"pasteData" = input$pasteData)
+		df = if (is.null(inFile)) {
+			return(NULL)
+		} else {
+			ptn = "\\.[[:alnum:]]{1,8}$"
+			suf = tolower(regmatches(inFile$name, regexpr(ptn, inFile$name)))
+			switch(suf,
+						 ".csv" = readr::read_csv(inFile$datapath),
+						 ".dta" = haven::read_dta(inFile$datapath),
+						 ".sav" = haven::read_sav(inFile$datapath),
+						 ".sas7bdat" = haven::read_sas(inFile$datapath),
+						 ".rds" = readRDS(inFile$datapath))
+		}
 
-		# Validate input
-		if (!is.null(df)) {
-			validate(
-				need("i" %in% names(df), "Replication ID 'i' not in the dataset"),
-				need("par" %in% names(df), "Estimand 'par' not in the dataset"),
-				need("coef" %in% names(df), "Estimated values 'coef' not in the dataset"),
-				need("se" %in% names(df), "Standard errors 'se' not in the dataset"),
-				need("true" %in% names(df), "True value for each estimand 'true' not in the dataset"),
-				need("method" %in% names(df), "Variable with methods to compare 'method' not in the dataset")
-			)}
-
-		# Make group (i.e. scenario) indicator variable
-		df = df %>%
-			group_by_at(.vars = names(df)[!(names(df) %in% c("i", "par", "coef", "se", "true", "method"))])
-		df$scenario = group_indices(df)
-		df = df %>%
-			ungroup() %>%
-			mutate(grpinternal = paste(scenario, par, sep = "_"))
+		# Return dataset
 		return(df)
-		})
-
-	# Make a data table with the original dataset
-	output$uploadedDataTable = renderDataTable({
-		data() %>%
-			select(-grpinternal, -scenario)
 	})
 
+	# Make variable selectors
+	output$defineVariables = shiny::renderUI({
+		shiny::validate(shiny::need(!is.null(data()), "Upload a dataset first."))
+		tagList(
+			div(style = "display: inline-block; vertical-align: top; width: 19%; min-width: 150px",
+					shiny::selectInput(inputId = "defineEstvarname",
+														 label = "Point estimates:",
+														 choices = names(data()))),
+			div(style = "display: inline-block; vertical-align: top; width: 19%; min-width: 150px",
+					shiny::selectInput(inputId = "defineSe",
+														 label = "Standard errors:",
+														 choices = names(data()))),
+			div(style = "display: inline-block; vertical-align: top; width: 19%; min-width: 150px",
+					shiny::numericInput(inputId = "defineTrue",
+															label = "True value:",
+															value = 0)),
+			div(style = "display: inline-block; vertical-align: top; width: 19%; min-width: 150px",
+					shiny::selectInput(inputId = "defineMethod",
+														 label = "Methods:",
+														 choices = names(data()),
+														 multiple = TRUE)),
+			div(style = "display: inline-block; vertical-align: top; width: 19%; min-width: 150px",
+					shiny::selectInput(inputId = "defineBy",
+														 label = "By factors:",
+														 choices = names(data()),
+														 multiple = TRUE))
+		)
+	})
+
+	# Make a data table with the original dataset
+	output$uploadedDataTable = shiny::renderDataTable({
+		shiny::validate(shiny::need(!is.null(data()), "Upload a dataset first, using the 'data' tab."))
+		data()
+	})
+
+	# Make a reference method selector for the summary statistics table
+	output$summaryRefMethodSelector = shiny::renderUI({
+		if (length(input$defineMethod) > 0) shiny::selectInput(inputId = "summaryRefMethodSelector",
+																													 label = "Select reference method:",
+																													 choices = unique(data()[[input$defineMethod[1]]]))
+	})
+
+	# Make summary statistics
+	summ = shiny::reactive({
+		s = rsimsum::simsum(data = data(),
+							 estvarname = input$defineEstvarname,
+							 true = input$defineTrue,
+							 se = input$defineSe,
+							 methodvar = input$defineMethod[1],
+							 ref = input$summaryRefMethodSelector)
+
+		return(s$summ)
+	})
+
+	# Make a data table with the summary statistics
+	output$summaryStatisticsDataTable = shiny::renderDataTable({
+		summ()
+	})
+
+	# Download data.frame with summary statistics
+	output$exportSummaryStatisticsButton = shiny::downloadHandler(
+		filename = function() {
+			extension = switch(input$exportSummaryStatisticsType,
+												 "csv" = ".csv",
+												 "tsv" = ".tsv",
+												 "r" = ".rds",
+												 "stata" = ".dta",
+												 "spss" = ".sav",
+												 "sas" = ".sas7bdat")
+			paste0(input$exportSummaryStatisticsName, extension)
+		},
+		content = function(file) {
+			if (input$exportSummaryStatisticsType == "csv") {
+				readr::write_csv(x = summ(), path = file)
+			} else if (input$exportSummaryStatisticsType == "tsv") {
+				readr::write_tsv(x = summ(), path = file)
+			} else if (input$exportSummaryStatisticsType == "r") {
+				saveRDS(object = summ(), file = file)
+			} else if (input$exportSummaryStatisticsType == "stata") {
+				haven::write_dta(data = summ(), path = file)
+			} else if (input$exportSummaryStatisticsType == "spss") {
+				haven::write_sav(data = summ(), path = file)
+			} else if (input$exportSummaryStatisticsType == "sas") {
+				haven::write_sas(data = summ(), path = file)
+			}
+		}
+	)
 }
-
-
-
