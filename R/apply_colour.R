902006c184be3ce1a4354853f0c433d0e068d914
Alessandro Gasparini <ag475@le.ac.uk>
2018-04-06 16:10:12 +0100

    Updated apply_theme and apply_colour functions

--- R/apply_colour.R
+++ R/apply_colour.R
@@ -1,40 +1,26 @@
 #' Apply colour to a ggplot object
 #'
 #' @param ggobject A `ggplot2` object.
-#' @param theme A string specifying the colour and fill scales to be added to the ggplot2 plot. Possible choices are: `theme_base`, `theme_bw`, `theme_calc`, `theme_classic`, `theme_cowplot`, `theme_dark`, `theme_economist`, `theme_excel`, `theme_few`, `theme_fivethirtyeight`, `theme_gdocs`, `theme_grey`, `theme_hc`, `theme_igray`, `theme_light`, `theme_linedraw`, `theme_minimal`, `theme_pander`, `theme_solarized`, `theme_stata_s2color`, `theme_stata_s2mono`, `theme_stata_s1color`, `theme_stata_s1rcolor`, `theme_stata_s1mono`, `theme_stata_s2manual`, `theme_stata_s1manual`, `theme_stata_sj`, `theme_tufte`, `theme_wsj`. Color and fill scales are imported from the `ggplot2`, `ggthemes`, and `cowplot` packages.
+#' @param theme A string specifying the theme to be added to the ggplot2 plot. Possible choices are: `theme_bw`, `theme_dark`, `theme_few`, `theme_grey`, `theme_light`, `theme_linedraw`, `theme_minimal`, `theme_tufte`, `theme_viridis`. Themes, color scales, and fill scales are imported from the `ggplot2`, `ggthemes`, `viridis`.
 #' @return A `ggplot2` object.
 apply_colour <- function(ggobject, theme) {
-  sc <- switch(
-    theme,
-    # 		"theme_base" = ,
-    # 		"theme_bw" = ,
-    "theme_calc" = ggthemes::scale_fill_calc(),
-    # 		"theme_classic" = ,
-    # 		"theme_cowplot" = ,
-    # 		"theme_dark" = ,
-    "theme_economist" = ggthemes::scale_color_economist(),
-    "theme_excel" = ggthemes::scale_fill_excel(),
-    "theme_few" = ggthemes::scale_color_few(),
-    "theme_fivethirtyeight" = ggthemes::scale_colour_fivethirtyeight(),
-    "theme_gdocs" = ggthemes::scale_fill_gdocs(),
-    # 		"theme_grey" = ,
-    "theme_hc" = ggthemes::scale_colour_hc(),
-    # 		"theme_igray" = ,
-    # 		"theme_light" = ,
-    # 		"theme_linedraw" = ,
-    # 		"theme_minimal" = ,
-    "theme_pander" = ggthemes::scale_color_pander(),
-    "theme_solarized" = ggthemes::scale_fill_solarized(),
-    "theme_stata_s2color" = ggthemes::scale_color_stata(scheme = "s2color"),
-    "theme_stata_s2mono" = ggthemes::scale_colour_stata(scheme = "s2mono"),
-    "theme_stata_s1color" = ggthemes::scale_colour_stata(scheme = "s1color"),
-    "theme_stata_s1rcolor" = ggthemes::scale_colour_stata(scheme = "s1rcolor"),
-    "theme_stata_s1mono" = ggthemes::scale_colour_stata(scheme = "s1mono"),
-    "theme_stata_s2manual" = ggthemes::scale_colour_stata(scheme = "s2manual"),
-    "theme_stata_s1manual" = ggthemes::scale_colour_stata(scheme = "s1manual"),
-    "theme_stata_sj" = ggthemes::scale_colour_stata(scheme = "sj"),
-    # 		"theme_tufte" = ,
-    "theme_wsj" = ggthemes::scale_colour_wsj()
+  return(
+    switch(
+      theme,
+      "theme_bw" = ggobject,
+      "theme_dark" = ggobject,
+      "theme_few" = ggobject + ggthemes::scale_color_few(),
+      "theme_grey" = ggobject,
+      "theme_light" = ggobject,
+      "theme_linedraw" = ggobject,
+      "theme_minimal" = ggobject,
+      "theme_tufte" = ggobject,
+      "theme_viridis" = ggobject + viridis::scale_color_viridis() + viridis::scale_fill_viridis()
+    )
   )
-  return(ggobject + sc)
+}
+
+#' @rdname apply_colour
+apply_color <- function(ggobject, theme) {
+	apply_colour(ggobject = ggobject, theme = theme)
 }
