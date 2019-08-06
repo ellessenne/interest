1ad2d0be59041296d89be25e74ff1f3fc197ccff
Alessandro Gasparini <ellessenne@gmail.com>
2019-08-06 15:47:14 +0100

    Added hex sticker

---
+++ inst/Sticker/make-sticker.R
@@ -0,0 +1,40 @@
+### R Script to make hex sticker
+library(hexSticker)
+# Using dev version of hexSticker from:
+# devtools::install_github("GuangchuangYu/hexSticker")
+library(sysfonts)
+
+sysfonts::font_add(family = "Iosevka Slab", regular = "iosevka-slab-regular.ttc")
+p_family <- "Iosevka Slab"
+
+# Convert .eps to .png
+# Requires imagemagick
+dpi <- 600
+system(paste0("convert -colorspace rgb -density ", dpi, " -units PixelsPerInch inst/Sticker/analytics.eps inst/Sticker/analytics.png"))
+
+hexSticker::sticker(
+  subplot = "inst/Sticker/analytics.png",
+  s_x = 1,
+  s_y = 1,
+  s_width = 2 / 3,
+  s_height = 2 / 3,
+  package = "",
+  p_color = "#000000",
+  p_family = p_family,
+  p_x = 1,
+  p_y = 2,
+  p_size = 6,
+  h_size = 5,
+  h_fill = "#FFFFFF",
+  h_color = "#99D0A7",
+  url = "INTEREST",
+  u_x = 0.935,
+  u_y = 0.16,
+  u_color = "#F1705E",
+  u_family = p_family,
+  u_size = 6,
+  filename = "inst/Sticker/interest.png",
+  dpi = dpi,
+  white_around_sticker = TRUE,
+  asp = 0.863
+)
